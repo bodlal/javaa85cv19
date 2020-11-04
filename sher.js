f71fdb7dc4d86011905b7a8008fdcbc1125a2685
bodlal <33960347+bodlal@users.noreply.github.com>
2020-11-04 23:52:15 +0100

    Update sher.js

--- sher.js
+++ sher.js
@@ -1,20 +1,26 @@
-var c = 0;
 
-$(document).ready(function () {
-	$("#b1")
-.on('click', function () {
-++c;
-if (c > 10) {
-$(this)
-.attr({
-href: "###",
-target: "_self"
-					});
-			}
-		});
-	$("#b2")
-		.on('click', function () {
-			if (c > 3) window.location = "dir link hna";
-			else window.alert("للأسف هناك اجراء مفقود لم تقم بالمشاركة مع 10 من اصدقائك " );
-		});
-});
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
